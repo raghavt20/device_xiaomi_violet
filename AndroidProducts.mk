9f807f71165aeb9c99099142abab51bbf6a739ec
raghavt20 <63952537+raghavt20@users.noreply.github.com>
2020-07-28 20:35:03 +0530

    violet: introduce Lineage -> POSP common lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_violet.mk
+    $(LOCAL_DIR)/potato_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_violet-userdebug \
-    lineage_violet-eng
+    potato_violet-userdebug \
+    potato_violet-eng

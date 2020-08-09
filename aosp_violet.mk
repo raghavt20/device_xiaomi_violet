697dff63c3305ada7c2eb4e5e5c94bf759d2e8b2
raghavt20 <raghavtandon2000@gmail.com>
2020-08-09 13:54:21 +0000

    violet: AOSP-ify

--- aosp_violet.mk
+++ aosp_violet.mk
@@ -8,10 +8,10 @@
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_violet
+PRODUCT_NAME := aosp_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
